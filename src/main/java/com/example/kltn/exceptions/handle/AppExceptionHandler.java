8312c17c623dc0bf1615f9e3cd74717f4915f0c7
quangdang <quangdang2001@gmail.com>
2023-04-16 12:57:39 +0700

    update handle exception

--- src/main/java/com/example/kltn/exceptions/handle/AppExceptionHandler.java
+++ src/main/java/com/example/kltn/exceptions/handle/AppExceptionHandler.java
@@ -1,6 +1,7 @@
 package com.example.kltn.exceptions.handle;
 
 import com.example.kltn.dto.ExceptionResq;
+import com.example.kltn.dto.ResponseDTO;
 import com.example.kltn.exceptions.*;
 import org.springframework.dao.DuplicateKeyException;
 import org.springframework.http.ResponseEntity;
@@ -12,35 +13,35 @@ public class AppExceptionHandler {
     @ExceptionHandler(UserException.class)
     private ResponseEntity<?> userException(UserException e) {
         return ResponseEntity.status(e.getCode())
-                .body(new ExceptionResq(e.getCode(), e.getMessage()));
+                .body(new ResponseDTO(false, e.getMessage(), e.getMessage()));
     }
 
     @ExceptionHandler(AuthException.class)
     private ResponseEntity<?> authException(AuthException e) {
         return ResponseEntity.status(e.getCode())
-                .body(new ExceptionResq(e.getCode(), e.getMessage()));
+                .body(new ResponseDTO(false, e.getMessage(), e.getMessage()));
     }
 
     @ExceptionHandler(AppException.class)
     private ResponseEntity<?> appException(AppException e) {
         return ResponseEntity.status(e.getCode())
-                .body(new ExceptionResq(e.getCode(), e.getMessage()));
+                .body(new ResponseDTO(false, e.getMessage(), e.getMessage()));
     }
 
     @ExceptionHandler(DuplicateKeyException.class)
     private ResponseEntity<?> duplicateException(DuplicateKeyException e) {
         return ResponseEntity.status(400)
-                .body(new ExceptionResq(400, e.getMessage()));
+                .body(new ResponseDTO(false, e.getMessage(), "Đã tồn tại thông tin nhập vào"));
     }
     @ExceptionHandler(NotFoundException.class)
     private ResponseEntity<?> notFoundException(NotFoundException e) {
         return ResponseEntity.status(404)
-                .body(new ExceptionResq(404, e.getMessage()));
+                .body(new ResponseDTO(false, e.getMessage(), e.getMessage()));
     }
 
     @ExceptionHandler(PaymentException.class)
     private ResponseEntity<?> paymentException(PaymentException e) {
         return ResponseEntity.status(404)
-                .body(new ExceptionResq(400, e.getMessage()));
+                .body(new ResponseDTO(false, e.getMessage(), e.getMessage()));
     }
 }
